c4fad321726864c4e9409cf59e3715628ad88bdd
Dan Kouba <ductpaemaster@gmail.com>
2019-02-03 22:48:36 -0800

    Support empty id in Measurement init as it is an autoincrementing field

--- Measurement.py
+++ Measurement.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Integer, TIMESTAMP, Float, ForeignKey
 
 # TODO: Add default for current timestamp in ts field.  Should be set client-side, but just in case
 
+
 class Measurement(Base):
 	__tablename__ = 'measurement'
 
@@ -14,7 +15,7 @@ class Measurement(Base):
 	def __repr__(self):
 		return "<Measurement(id={}, ts={}, mtype='{}', data={})>".format(self.id, self.ts, self.mtype, self.data)
 
-	def __init__(self, id, ts, mtype, data):
+	def __init__(self, ts, mtype, data, id=None):
 		self.id = id
 		self.ts = ts
 		self.mtype = mtype
