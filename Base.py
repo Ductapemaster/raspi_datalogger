abb1cc1e865f6f09d0c3e493a22072f89860a423
Dan Kouba <ductpaemaster@gmail.com>
2019-02-09 10:23:03 -0800

    Use secrets file to hold database url info

--- Base.py
+++ Base.py
@@ -1,9 +1,10 @@
-import secrets
+from secrets import sql_database_url
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql://{}:{}@localhost/datalogger".format(secrets.sql_username, secrets.sql_password))
+
+engine = create_engine(sql_database_url)
 Session = sessionmaker(bind=engine)
 session = Session()
 
--- secrets.py
+++ secrets.py
@@ -1,2 +1,6 @@
 sql_username = "<username>"
 sql_password = "<password>"
+sql_database_server = "localhost"
+sql_database_name = "datalogger"
+
+sql_database_url = "mysql://{}:{}@{}/{}".format(sql_username, sql_password, sql_database_server, sql_database_name)
