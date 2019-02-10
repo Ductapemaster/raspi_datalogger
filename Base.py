e9bbf44595b6844ebb5c872828e8c21c19d96cf8
Dan Kouba <ductpaemaster@gmail.com>
2019-02-10 00:33:00 -0800

    Attempt to fix the MySQL server has gone away issue
    Session seems to close automatically after not being used for a long time.  Hopefully this fixes.

--- Base.py
+++ Base.py
@@ -1,11 +1,11 @@
 from secrets import sql_database_url
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-
+# Trying fix for "mysql server has gone away" exception:
+# https://stackoverflow.com/questions/6471549/avoiding-mysql-server-has-gone-away-on-infrequently-used-python-flask-server
 engine = create_engine(sql_database_url)
-Session = sessionmaker(bind=engine)
-session = Session()
+Session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
